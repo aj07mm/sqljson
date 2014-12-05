13cc36182808f4bbc89d4b08467d6a4b7cffbb5e
aj07mm <aj07mm@hotmail.com>
2014-12-04 23:13:52 -0200

    funcionando no browser e não no mocha~

--- test/q_spec.js
+++ test/q_spec.js
@@ -3,48 +3,15 @@ var assert = require('assert');
 var q = require('../q.js');
 
 describe('q',function(){
-	/*context('3 columns, 1 table', function(){
-        it('should return 1', function(){
-
-        	selectColumns = ['id','title'];
-
-            table = [{
-				'id':'asd',
-				'title':'asdasdas',
-				'content':'aosndoaksnda'
-			}];
-
-		    assert.equal(
-	    		q.select(selectColumns).from(table), 
-	    		[{'id': 'asd'},{'title':'asdasdas'}]
-		    );
-
-	    });
-   	});*/
-
-	context('select',function(){
-		it('test select id',function(){
-			cols = ['id'];
-
-            table = [{
-				'id':'asd'
-			}];
-
-		    assert.equal(
-	    		q.select(cols), cols
-		    );
-		})
-	});
-
-	/*context('from',function(){
+	context('from',function(){
 		it('test from id',function(){
 			cols = ['id'];
 
-            table = [{id:123},{id:777}];
-            console.log(q)
-		    assert.equal( q.select(cols).from(table), [{id:123},{id:777}]);
+            fooTable = [{id:123},{id:777}];
+       
+		    assert.equal( q.select(cols).from(fooTable), [{id:123},{id:777}]);
 		})
-	});*/
+	});
 	
 
 });
